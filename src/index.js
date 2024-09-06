ad0948dc0fe0d2b75507d5f29a424b2eecf2783b
Charlene-Kwangware <harleykay670@gmail.com>
2024-09-06 17:02:53 +0200

    added weather search function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function displayTemperature(response) {
+  let temperatureValue = document.querySelector("#temperature-value");
+  let temp = Math.round(response.data.temperature.current);
+  temperatureValue.innerHTML = temp;
+  let cityNameShow = document.querySelector("h1");
+  cityNameShow.innerHTML = response.data.city;
+}
+
+function searchCityWeather(city) {
+  let apiKey = "56aadbfcfa63b7ofa0333tf649578e7b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-name-form");
-  let cityNameShow = document.querySelector("h1");
-  cityNameShow.innerHTML = searchInput.value;
+
+  searchCityWeather(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
